82899728cd288aff1771c3f02228c047837be8f7
Dariusz Niemczyk <palid@o2.pl>
2017-07-24 21:16:25 +0200

    Make tap-notify work with tap-watch.

--- distributions/index.js
+++ distributions/index.js
@@ -22,6 +22,10 @@ var _path = require('path');
 
 var _path2 = _interopRequireDefault(_path);
 
+var _lodash = require('lodash.debounce');
+
+var _lodash2 = _interopRequireDefault(_lodash);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var DEFAULT_PASSED_OPTIONS = {
@@ -36,21 +40,99 @@ var DEFAULT_FAILED_OPTIONS = {
   sound: 'Basso'
 };
 
+function extractFailureLines(pollutedTap) {
+  return [].concat(jsErrorLines(pollutedTap)).concat(tapFailureLines(pollutedTap));
+}
+
+// bc stderr has been merged with stdout
+// we may not have pure TAP output.
+// this will extract syntax or runtime errors
+function jsErrorLines(pollutedTap) {
+  return pollutedTap.split("\n").filter(function (x) {
+    return x.match(jsErrorRegex());
+  });
+}
+
+function tapFailureLines(pollutedTap) {
+  return pollutedTap.split("\n").filter(function (x) {
+    return x.match(/^not ok|    at:/);
+  }).map(function (x) {
+    return x.replace(/^not ok \d+/, '');
+  }).map(function (x) {
+    return x.replace(/^    at:.*\/([^\/]+)\)/, 'at $1');
+  });
+}
+
+// some funky stuff to make newlines work right in growl
+function growlFormat(lines) {
+  return lines.map(function (x) {
+    return '"' + x + '"';
+  }).join("$'\n'");
+}
+
+function jsErrorRegex() {
+  return (/^\w+Error:|    at /
+  );
+}
+
+function getTestsAmount(stdin) {
+  var match = stdin.match(/# tests +(\d+)/);
+  if (!match) return null;
+  var testsAmount = parseInt(match[1], 10);
+  return testsAmount;
+}
+
+function getPassedTests(stdin) {
+  var match = stdin.match(/# pass +(\d+)/);
+  if (!match) {
+    return 0;
+  }
+  var testsAmount = parseInt(match[1], 10);
+  return testsAmount;
+}
+
+function getFailedTests(stdin) {
+  var match = stdin.match(/# fail +(\d+)/);
+  if (!match) {
+    return 0;
+  }
+  var testsAmount = parseInt(match[1], 10);
+  return testsAmount;
+}
+
 var createReporter = function createReporter() {
-  var _ref = arguments.length <= 0 || arguments[0] === undefined ? {} : arguments[0];
+  var _ref = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {},
+      passed = _ref.passed,
+      failed = _ref.failed;
 
-  var passed = _ref.passed;
-  var failed = _ref.failed;
-
+  var debouncedNotify = (0, _lodash2.default)(_nodeNotifier2.default.notify.bind(_nodeNotifier2.default), 100);
   var passedOptions = _extends({}, DEFAULT_PASSED_OPTIONS, passed);
   var failedOptions = _extends({}, DEFAULT_FAILED_OPTIONS, failed);
   var p = (0, _tapParser2.default)();
+  var stdin = '';
   var stream = (0, _through2.default)(function (chunk, enc, next) {
+    stdin += chunk;
+    var testsAmount = getTestsAmount(stdin);
+
+    if (testsAmount) {
+      var failedTests = getFailedTests(stdin);
+      var passedTests = getPassedTests(stdin);
+      var failureLines = extractFailureLines(stdin);
+      stdin = '';
+      if (failedTests > 0) {
+        debouncedNotify(_extends({
+          message: failedTests + ' of ' + testsAmount + ' tests failed!' + failureLines
+        }, failedOptions));
+      } else if (passedTests > 0 && testsAmount > 0) {
+        debouncedNotify(_extends({
+          message: passedTests + ' of ' + testsAmount + ' tests passed!'
+        }, passedOptions));
+      }
+    }
     this.push(chunk);
     next();
   });
   var errorOccuredAt = null;
-
   stream.pipe(p);
 
   p.on('assert', function (assert) {
@@ -61,11 +143,11 @@ var createReporter = function createReporter() {
 
   p.on('complete', function (result) {
     if (result.ok) {
-      _nodeNotifier2.default.notify(_extends({
+      debouncedNotify(_extends({
         message: result.pass + ' of ' + result.count + ' tests passed.'
       }, passedOptions));
     } else {
-      _nodeNotifier2.default.notify(_extends({
+      debouncedNotify(_extends({
         message: (result.fail || 0) + ' of ' + result.count + ' tests failed' + (errorOccuredAt ? ' at ' + errorOccuredAt : '')
       }, failedOptions));
     }
--- sources/index.js
+++ sources/index.js
@@ -2,6 +2,7 @@ import notifier from 'node-notifier';
 import through2 from 'through2';
 import parser from 'tap-parser';
 import path from 'path';
+import debounce from 'lodash.debounce';
 
 const DEFAULT_PASSED_OPTIONS = {
   title: 'Test passed.',
@@ -15,16 +16,92 @@ const DEFAULT_FAILED_OPTIONS = {
   sound: 'Basso',
 };
 
-const createReporter = ({ passed, failed } = {}) => {
+function extractFailureLines(pollutedTap) {
+  return [].concat(jsErrorLines(pollutedTap))
+    .concat(tapFailureLines(pollutedTap));
+}
+
+// bc stderr has been merged with stdout
+// we may not have pure TAP output.
+// this will extract syntax or runtime errors
+function jsErrorLines(pollutedTap) {
+  return pollutedTap.split("\n").filter(x => x.match(jsErrorRegex()));
+}
+
+function tapFailureLines(pollutedTap) {
+  return pollutedTap
+    .split("\n")
+    .filter(x => x.match(/^not ok|    at:/))
+    .map(x => x.replace(/^not ok \d+/, ''))
+    .map(x => x.replace(/^    at:.*\/([^\/]+)\)/, 'at $1'))
+}
+
+// some funky stuff to make newlines work right in growl
+function growlFormat(lines) {
+  return lines.map(x => `"${x}"`).join("$'\n'");
+}
+
+function jsErrorRegex() {
+  return /^\w+Error:|    at /
+}
+
+function getTestsAmount(stdin) {
+  const match = stdin.match(/# tests +(\d+)/);
+  if (!match) return null;
+  const testsAmount = parseInt(match[1], 10);
+  return testsAmount;
+}
+
+function getPassedTests(stdin) {
+  const match = stdin.match(/# pass +(\d+)/);
+  if (!match) {
+    return 0;
+  }
+  const testsAmount = parseInt(match[1], 10);
+  return testsAmount;
+}
+
+function getFailedTests(stdin) {
+  const match = stdin.match(/# fail +(\d+)/);
+  if (!match) {
+    return 0;
+  }
+  const testsAmount = parseInt(match[1], 10);
+  return testsAmount;
+}
+
+const createReporter = ({
+  passed,
+  failed
+} = {}) => {
+  const debouncedNotify = debounce(notifier.notify.bind(notifier), 100);
   const passedOptions = Object.assign({}, DEFAULT_PASSED_OPTIONS, passed);
   const failedOptions = Object.assign({}, DEFAULT_FAILED_OPTIONS, failed);
   const p = parser();
+  let stdin = '';
   const stream = through2(function(chunk, enc, next) {
+    stdin += chunk;
+    const testsAmount = getTestsAmount(stdin);
+
+    if (testsAmount) {
+      const failedTests = getFailedTests(stdin);
+      const passedTests = getPassedTests(stdin);
+      const failureLines = extractFailureLines(stdin);
+      stdin = '';
+      if (failedTests > 0) {
+        debouncedNotify(Object.assign({
+          message: `${failedTests} of ${testsAmount} tests failed!` + failureLines
+        }, failedOptions));
+      } else if (passedTests > 0 && testsAmount > 0){
+        debouncedNotify(Object.assign({
+          message: `${passedTests} of ${testsAmount} tests passed!`,
+        }, passedOptions));
+      }
+    }
     this.push(chunk);
     next();
   });
   let errorOccuredAt = null;
-
   stream.pipe(p);
 
   p.on('assert', assert => {
@@ -35,13 +112,13 @@ const createReporter = ({ passed, failed } = {}) => {
 
   p.on('complete', result => {
     if (result.ok) {
-      notifier.notify(Object.assign({
+      debouncedNotify(Object.assign({
         message: `${result.pass} of ${result.count} tests passed.`,
       }, passedOptions));
     } else {
-      notifier.notify(Object.assign({
+      debouncedNotify(Object.assign({
         message: `${result.fail || 0} of ${result.count} tests failed` +
-                 (errorOccuredAt ? ` at ${errorOccuredAt}` : ''),
+          (errorOccuredAt ? ` at ${errorOccuredAt}` : ''),
       }, failedOptions));
     }
   });
